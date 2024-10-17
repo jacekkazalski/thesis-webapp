a46c68c94d91a186eb29c822fd7c6714e952bb88
jacekkazalski <jacek.kazalski@gmai.com>
2024-10-17 19:58:33 +0200

    navbar css

--- frontend/src/components/layout/NavBar.tsx
+++ frontend/src/components/layout/NavBar.tsx
@@ -5,10 +5,18 @@ import styles from "./NavBar.module.css";
 export default function NavBar(){
     return(
         <div className={styles.navbar}>
-            <h2>Co w lodówce</h2>
-            <TextInput label={"Szukaj"} type={"text"} placeholder={"Szukaj..."}/>
-            <Button variant={"primary"} text={"Logowanie"} type={"button"}/>
-            <Button variant={"primary"} text={"Rejestracja"} type={"button"}/>
+            <div className={styles.logo}>
+                <h2>Co w lodówce</h2>
+            </div>
+            <div className={styles.searchbar}>
+                <TextInput label={"Szukaj"} type={"text"} placeholder={"Szukaj..."}/>
+            </div>
+
+            <div className={styles.buttons}>
+                <Button variant={"primary"} text={"Logowanie"} type={"button"}/>
+                <Button variant={"primary"} text={"Rejestracja"} type={"button"}/>
+            </div>
+
         </div>
     )
 }
