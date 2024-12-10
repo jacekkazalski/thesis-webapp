1ea252f48fb5c5226255d03583fd05ade3b83b80
jacekkazalski <jacek.kazalski@gmai.com>
2024-12-10 12:01:10 +0100

    db connection test

---
+++ backend/src/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const { Pool } = require('pg');
+const port = process.env.PORT || 3000;
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'recipedb',
+    password: 'postgres',
+    port: 5432
+})
+
+// Connection testing
+pool.connect((err, client, release) => {
+    if (err) {
+        console.error('Error acquiring client', err.stack);
+    } else {
+        console.log('Connected to the database');
+    }
+    release();
+});
+
+app.get("/recipes", async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM public."Recipe"');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send(err.message);
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
