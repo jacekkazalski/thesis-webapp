787e50a02c881788cbf19b29f9c5b70297648a8d
jacekkazalski <jacek.kazalski@gmai.com>
2024-12-11 14:51:01 +0100

    login controller

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,5 +1,12 @@
 const user = require('../db/models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const generateToken = (payload) => {
+    return jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE_IN,
+    })
+}
 
 const signup = async (req, res, next) => {
     const {username, email, password, confirmPassword} = req.body;
@@ -32,11 +39,59 @@ const signup = async (req, res, next) => {
             message: 'Failed to create the user'
         })
     }
+
+    const token = generateToken({id: newUser.id_user, username: newUser.username})
     return res.status(201).json({
         status: 'success',
         message: 'User created successfully',
-        data: newUser
+        data: {
+            user: {
+                id: newUser.id_user,
+                username: newUser.username,
+                email: newUser.email,
+            },
+            token
+        }
     })
 }
 
-module.exports = {signup}
+const login = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'Missing required fields'
+        })
+    }
+
+    const result = await user.findOne({
+        where: {email: email},
+    })
+    if(!result || !await bcrypt.compare(password, result.password) ) {
+        return res.status(404).json({
+            status: 'error',
+            message: 'Invalid login data'
+        })
+    }
+
+    const token = generateToken({
+        id: result.id_user,
+        username: result.username,
+    })
+
+    return res.status(200).json({
+        status: 'success',
+        message: 'Login successful',
+        data: {
+            user: {
+                id: result.id_user,
+                username: result.username,
+                email: result.email,
+            },
+            token,
+        },
+    })
+}
+
+module.exports = {signup, login}
