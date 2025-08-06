a55cae9909db4d56723bee9a7acd1e9e23ede9f4
jacekkazalski <jacek.kazalski@gmail.com>
2025-08-06 12:50:30 +0200

    feat: Gallery viewType buttons

--- frontend/src/components/Gallery.tsx
+++ frontend/src/components/Gallery.tsx
@@ -2,14 +2,22 @@ import {useEffect, useState} from "react";
 import RecipeCard from "./common/RecipeCard.tsx";
 import axios from "../api/axios.ts";
 import {Recipe} from "../utils/types.ts";
-import {Box, Grid, Stack, MenuItem, Select, FormControl, FormHelperText} from "@mui/material";
+import {
+    Box, Grid, Stack, MenuItem, Select, FormControl, FormHelperText, ButtonGroup, Button, ToggleButtonGroup,
+    ToggleButton, Tooltip
+} from "@mui/material";
+import {FormatListBulleted, ViewListOutlined, ViewModule, ViewModuleOutlined} from "@mui/icons-material";
 
 export default function Gallery(){
     const [viewType, setViewType] = useState<"gallery" | "list">("gallery")
     const [recipes, setRecipes] = useState<Recipe[]>([]);
     const [sortBy, setSortBy] = useState("rating")
     //TODO: Pagination
-
+    const handleViewTypeChange = (_event: any, newViewType: string | ((prevState: "gallery" | "list") => "gallery" | "list") | null) => {
+        if (newViewType!=null) {
+            setViewType(newViewType)
+        }
+    }
     useEffect(() => {
         const fetchRecipes = async () => {
             const response = await axios.get("/recipes", { params: { sortBy } });
@@ -20,8 +28,9 @@ export default function Gallery(){
     }, [sortBy]);
     return(
         <Box>
-            <Box>
-                <Stack direction="row" spacing={1}>
+            <Box p={2}>
+                <Stack direction="row" spacing={2}>
+
                     <FormControl size="small" sx={{ minWidth: 160 }}>
                         <Select
                             value={sortBy}
@@ -34,6 +43,16 @@ export default function Gallery(){
                         </Select>
                         <FormHelperText>Sortuj przepisy</FormHelperText>
                     </FormControl>
+                    <Stack direction="column"><ToggleButtonGroup value={viewType} exclusive size="small" onChange={handleViewTypeChange}>
+                        <Tooltip title="Galeria"><ToggleButton value="gallery">
+                            <ViewModule/>
+                        </ToggleButton></Tooltip>
+                        <Tooltip title="Lista"><ToggleButton value={"list"}>
+                            <FormatListBulleted/>
+                        </ToggleButton></Tooltip>
+                    </ToggleButtonGroup>
+                        <FormHelperText>Sposób wyświetlania</FormHelperText></Stack>
+
                 </Stack>
             </Box>
             <Grid container spacing={2}>
