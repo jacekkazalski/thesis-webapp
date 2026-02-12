09f5791b40bd5ea2d5603deee32cc404e2ab3e88
jacekkazalski <jacek.kazalski@gmail.com>
2026-02-12 19:19:43 +0100

    eslint + prettier config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,50 +3,55 @@ import js from '@eslint/js'
 import reactHooks from 'eslint-plugin-react-hooks'
 import ts from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
-import globals from "globals";
+import globals from 'globals'
+
+import prettierPlugin from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
-    {
-        files: ['src/**/*.{js,jsx,ts,tsx}'],
-        languageOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module',
-            parser: tsParser,
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true
-                },
-                project: './tsconfig.json',
-                tsconfigRootDir: process.cwd()
-            },
-            globals: {
-                ...globals.browser,
-                ...globals.es2021
-            },
-        },
+  {
+    files: ['src/**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.json',
+        tsconfigRootDir: process.cwd(),
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+      'react-hooks': reactHooks,
+      react,
+      prettier: prettierPlugin,
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...ts.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
 
-        plugins: {
-            '@typescript-eslint': ts,
-            'react-hooks': reactHooks,
-            react: react,
-        },
-        settings: {
-            react: {
-                version: 'detect'
-            },
-        },
-        rules: {
-            ...js.configs.recommended.rules,
-            ...ts.configs.recommended.rules,
-            ...react.configs.recommended.rules,
-            ...react.configs['jsx-runtime'].rules,
-            ...reactHooks.configs.recommended.rules,
-            '@typescript-eslint/no-unused-vars': [
-                'error',
-                {argsIgnorePattern: '^_', varsIgnorePattern: '^_'},
-            ],
-            'react/react-in-jsx-scope': 'off',
-            'react/prop-types': 'off',
-        }
-    }
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+
+    
+      'prettier/prettier': 'warn',
+    },
+  },
+
+  eslintConfigPrettier,
 ]
